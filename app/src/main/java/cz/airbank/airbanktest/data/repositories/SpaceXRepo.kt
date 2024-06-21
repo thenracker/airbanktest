d01e90adf0671767ba03938f163af7884ddd6a7b
Petr Weissar <weissar.petr@gmail.com>
2024-06-21 10:39:39 +0200

    SpaceXRepo logics fetch/select

--- app/src/main/java/cz/airbank/airbanktest/data/repositories/SpaceXRepo.kt
+++ app/src/main/java/cz/airbank/airbanktest/data/repositories/SpaceXRepo.kt
@@ -1,20 +1,26 @@
 package cz.airbank.airbanktest.data.repositories
 
 import cz.airbank.airbanktest.data.SpaceXApi
+import cz.airbank.airbanktest.data.datastore.Storage
 import cz.airbank.airbanktest.data.db.daos.LaunchDao
 import cz.airbank.airbanktest.data.db.entities.toEntity
 import cz.airbank.airbanktest.data.db.entities.toTO
 import cz.airbank.airbanktest.data.dto.LaunchTO
-import kotlin.random.Random
 
 class SpaceXRepo(
     private val api: SpaceXApi,
     private val launchDao: LaunchDao,
+    private val storage: Storage,
 ) {
 
     suspend fun fetchOrSelectLaunches(): List<LaunchTO> {
-        // TODO decide by timestamp (older than 1 minute, fetch from API)
-        return if (Random.nextBoolean()) {
+
+        val currentMillis = System.currentTimeMillis()
+        val lastMillis = storage.getLaunchesTimestamp()
+
+        val shouldRefresh = (currentMillis - RefreshValidity) > lastMillis
+
+        return if (shouldRefresh) {
             fetchLaunches()
         } else {
             selectLaunches()
@@ -31,9 +37,12 @@ class SpaceXRepo(
         launchEntities.forEach { entity ->
             launchDao.insertOrUpdate(entity)
         }
-        // TODO save timestamp
+        storage.setLaunchesTimestamp(System.currentTimeMillis())
     }
 
     suspend fun fetchRocketDetail(id: String) = api.fetchRocketDetail(rocketId = id)
 
+    companion object {
+        private const val RefreshValidity = 5 * 60 * 1_000L // 5 minutes
+    }
 }
